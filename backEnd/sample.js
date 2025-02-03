6ccebd2b3be7535f6c0b3222974d6fc1e7d724a6
Verbrugge <90406459+Louis-Verbrugge@users.noreply.github.com>
2025-02-03 12:52:53 +0100

    finished filter countriesand beggin animal list

--- backEnd/sample.js
+++ backEnd/sample.js
@@ -2,10 +2,21 @@
 const express = require('express');
 const app = express();
 
+
+async function getApiWithAuthorization(pathURL) {
+    const response = await fetch(pathURL, {
+        headers: {
+            'accept: */*'
+            'Authorization': 'hRqyySeXuW3g5a7eu8ESrcDP4QhVDNTgZ7vb'
+        }
+    });
+    const data = await response.json();
+    return data;
+}
+
 async function getApi(pathURL) {
     const response = await fetch(pathURL);
     const data = await response.json();
-    console.log(data);
     return data;
 }
 
@@ -13,24 +24,29 @@ app.get('/', (req, res) => {
   res.send("hello world");
 });
 
-app.get('/country', (req, res) => {
+
+app.get('/animals', (req, res) => {
+
+    let listTemp = [];
+
     getApi('https://restcountries.com/v3.1/all?fields=name,translations,cca2').then(data => {
         data.sort((a, b) => a.name.common.localeCompare(b.name.common));
 
         // search details of a country
         for (let i = 0; i < data.length; i++) {
 
-            console.log(data[i].cca2);
             
-
+            getApiWithAuthorization(`https://api.iucnredlist.org/api/v4/countries/${data[i].cca2}`).then(countryData => {
+                console.log(countryData);
+            });
         }
 
         res.send(data);
     });
 });
 
-app.get('/animals', (req, res) => {
-    getApi('').then(data => {
+app.get('/country', (req, res) => {
+ getApi('https://restcountries.com/v3.1/all?fields=name,translations,cca2').then(data => {
         data.sort((a, b) => a.name.common.localeCompare(b.name.common));
         res.send(data);
     });
