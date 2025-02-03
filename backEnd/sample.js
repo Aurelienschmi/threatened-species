f5291353ed5db0641a4de0c90e3dc09f1faf3bb4
Verbrugge <90406459+Louis-Verbrugge@users.noreply.github.com>
2025-02-03 14:21:06 +0100

    begginin front + continue back

--- backEnd/sample.js
+++ backEnd/sample.js
@@ -1,56 +1,70 @@
-
+const axios = require('axios');
 const express = require('express');
 const app = express();
 
 
+
 async function getApiWithAuthorization(pathURL) {
-    const response = await fetch(pathURL, {
-        headers: {
-            'accept: */*'
-            'Authorization': 'hRqyySeXuW3g5a7eu8ESrcDP4QhVDNTgZ7vb'
-        }
-    });
-    const data = await response.json();
-    return data;
+  const response = await axios.get(pathURL, {
+    headers: {
+      'accept': 'application/json',
+      'Authorization': 'hRqyySeXuW3g5a7eu8ESrcDP4QhVDNTgZ7vb'
+    }
+  });
+  return response.data;
 }
 
 async function getApi(pathURL) {
-    const response = await fetch(pathURL);
-    const data = await response.json();
-    return data;
+  const response = await axios.get(pathURL);
+  return response.data;
 }
 
 app.get('/', (req, res) => {
   res.send("hello world");
 });
 
+app.get('/animal-FR', (req, res) => {
+    getApiWithAuthorization('https://api.iucnredlist.org/api/v4/countries/FR').then(data => {
+        res.send(data);
+    })
+});
+
 
 app.get('/animals', (req, res) => {
 
-    let listTemp = [];
-
     getApi('https://restcountries.com/v3.1/all?fields=name,translations,cca2').then(data => {
         data.sort((a, b) => a.name.common.localeCompare(b.name.common));
+        let dataCountryCode = [];
 
-        // search details of a country
-        for (let i = 0; i < data.length; i++) {
 
-            
-            getApiWithAuthorization(`https://api.iucnredlist.org/api/v4/countries/${data[i].cca2}`).then(countryData => {
-                console.log(countryData);
-            });
-        }
+        getApiWithAuthorization(`https://api.iucnredlist.org/api/v4/countries/FR`).then(dataAu => {
+            console.log(dataAu);
+        });
+
+        // data.map(country => {
+        //     dataCountryCode.push(getApiWithAuthorization(`https://api.iucnredlist.org/api/v4/countries/${country.cca2}`));
+        //     res.send(dataCountryCode);
+        // });
+
+        console.log("promises");
+        dataCountryCode.map((item, index) => {
+            console.log(item);
+        });
 
         res.send(data);
-    });
+    })
 });
 
+
+    
 app.get('/country', (req, res) => {
- getApi('https://restcountries.com/v3.1/all?fields=name,translations,cca2').then(data => {
+    getApi('https://restcountries.com/v3.1/all?fields=name,translations,cca2').then(data => {
         data.sort((a, b) => a.name.common.localeCompare(b.name.common));
         res.send(data);
     });
 });
+
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
--- readme.md
+++ readme.md
@@ -1,26 +1,11 @@
 <h1 align="center">Welcome to threatened-species 👋</h1>
 <p>
-  <img alt="Version" src="https://img.shields.io/badge/version-0.0.1-blue.svg?cacheSeconds=2592000" />
 </p>
-
-> threatened-species create with vite 
 
 ## Install
 
 ```sh
-npm i
-```
-
-## Usage
-
-```sh
-npm main
-```
-
-## Run tests
-
-```sh
-npm test
+npm install
 ```
 
 ## Show your support
