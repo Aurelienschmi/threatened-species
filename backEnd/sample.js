c501a947718fa792807dc55ac438f1d4b96840b0
Verbrugge <90406459+Louis-Verbrugge@users.noreply.github.com>
2025-02-03 16:50:26 +0100

    front (functional) + readme (begginning) + back (almost finised)

--- backEnd/sample.js
+++ backEnd/sample.js
@@ -2,15 +2,19 @@ const axios = require('axios');
 const express = require('express');
 const app = express();
 
-
+const path = require('path');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../frontEnd')); 
+app.use(express.static(path.join(__dirname, '../frontEnd')));
 
 async function getApiWithAuthorization(pathURL) {
   const response = await axios.get(pathURL, {
-    headers: {
-      'accept': 'application/json',
+    headers: {  
+      'accept': 'application/json', 
       'Authorization': 'hRqyySeXuW3g5a7eu8ESrcDP4QhVDNTgZ7vb'
     }
   });
+  
   return response.data;
 }
 
@@ -20,14 +24,25 @@ async function getApi(pathURL) {
 }
 
 app.get('/', (req, res) => {
-  res.send("hello world");
+    res.render("index");
+
+    getApi('https://restcountries.com/v3.1/all?fields=name,translations,cca2').then(data => {
+        data.sort((a, b) => a.name.common.localeCompare(b.name.common));
+        const updatedData = data.map(country => {
+            return {
+            ...country,
+            translationFra: country.translations?.fra?.common || ''
+            };
+        });
+        console.log("updatedData")
+        console.log(updatedData.map(country => country.name.common))
+        console.log("ççççç")
+        res.render('index', {apiPays: updatedData.map(country => country.name.common)});
+    });
+    
 });
 
-app.get('/animal-FR', (req, res) => {
-    getApiWithAuthorization('https://api.iucnredlist.org/api/v4/countries/FR').then(data => {
-        res.send(data);
-    })
-});
+
 
 app.get('/test', async (req, res) => {
   try {
@@ -38,57 +53,71 @@ app.get('/test', async (req, res) => {
   }
 });
 
-app.get('/animals', (req, res) => {
-
-    getApi('https://restcountries.com/v3.1/all?fields=name,translations,cca2').then(data => {
-        data.sort((a, b) => a.name.common.localeCompare(b.name.common));
-        let dataCountryCode = [];
-
-
-        data.map((item, index) => { 
-
-            getApiWithAuthorization(`https://api.iucnredlist.org/api/v4/countries/FR`).then(dataAu => {
-                dataCountryCode.push(dataAu);
-            });
-
-        });
-
-
-        setTimeout(() => {
-            dataCountryCode.map((item, index) => {
-                console.log(item);
-                res.send(item);
-            });
-        }, 3000);
-
-        // data.map(country => {
-        //     dataCountryCode.push(getApiWithAuthorization(`https://api.iucnredlist.org/api/v4/countries/${country.cca2}`));
-        //     res.send(dataCountryCode);
-        // });
-
-        console.log("promises");
-        console.log(dataCountryCode)
-        
-
-
-        console.log("promises");
-        dataCountryCode.map((item, index) => {
-            console.log(item);
-        });
-
-
-
-        res.send(dataCountryCode);
+app.get('/animal-FR', (req, res) => {
+    getApiWithAuthorization('https://api.iucnredlist.org/api/v4/countries/FR').then(data => {
+    res.send(data);
     })
 });
 
 
-    
+
+app.get('/animals/:pays', async (req, res) => {
+    const pays = req.params.pays;
+    let listAnimal = [];
+    try {
+      const data = await getApiWithAuthorization(`https://api.iucnredlist.org/api/v4/countries/${pays}`);
+      const assessmentIds = data.assessments.map(item => item.assessment_id);
+
+      assessmentIds.map(async (idAnimal) => {
+        listAnimal.push(await findAnimalWithId(idAnimal));
+      });
+
+      listAnimal.map((animal) => {
+        console.log(animal);
+      });
+      res.json(listAnimal);
+      
+    } catch (error) {
+      if (error.response) {
+        if (error.response.status === 429) {
+          res.status(429).send('Too Many Requests: Rate limit exceeded');
+        } else {
+          res.status(error.response.status).send(`Error: ${error.response.statusText}`);
+        }
+      } else {
+        res.status(500).send('Server error');
+      }
+    }
+});
+
+
+
+async function findAnimalWithId(idAnimal) {
+    try {
+        const dataAnimal = await getApiWithAuthorization(`https://api.iucnredlist.org/api/v4/assessment/${idAnimal}`);
+        console.log(dataAnimal.taxon.scientific_name);
+        return dataAnimal.taxon.scientific_name;
+    } catch (error) {
+        console.log('Server error');
+        return null;
+    }
+}
+
+
+  
 app.get('/country', (req, res) => {
     getApi('https://restcountries.com/v3.1/all?fields=name,translations,cca2').then(data => {
-        data.sort((a, b) => a.name.common.localeCompare(b.name.common));
-        res.send(data);
+    data.sort((a, b) => a.name.common.localeCompare(b.name.common));
+    const updatedData = data.map(country => {
+        return {
+        ...country,
+        translationFra: country.translations?.fra?.common || ''
+        };
     });
+
+
+    res.send(updatedData);
+});
 });
 
 
--- readme.md
+++ readme.md
@@ -1,6 +1,24 @@
-<h1 align="center">Welcome to threatened-species 👋</h1>
-<p>
-</p>
+# Welcome to Threatened Species 👋
+![Version](https://img.shields.io/badge/version-V1-blue.svg?cacheSeconds=2592000)
+[![Documentation](https://img.shields.io/badge/documentation-yes-brightgreen.svg)](https://www.iucnredlist.org/)
+
+> We try to create an API to have all threatened species by country order by Alphabet.
+
+> We have a list of countries ordered by Alphabet and each country have a list of species threatened also ordered by Alphabet.
+
+> We have created a graphic page for a better understanding.
+
+> We have separated the BackEnd and the Frontend in two different cases.
+
+> The BackEnd manages the recuperation of all the datas (countries https://restcountries.com/v3.1/all?fields=name,translations,cca2 and spieces threatened species by countries iucnredlist.org/api/v4/countries/).
+
+> The FrontEnd manages the display for users
+
+> 
+
+### 🏠 [Homepage](index.ejs)
+
+### ✨ [Demo](http://10.0.10.42:3000/)
 
 ## Install
 
@@ -8,9 +26,15 @@
 npm install
 ```
 
+## Author
+
+👤 **Gabin, Louis,Ziyad,Ambre**
+
+
 ## Show your support
 
 Give a ⭐️ if this project helped you!
+
 
 ***
 _This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
